ba0b248ddf8b5a84210171170602429d0e588d9d
d3xt3er <48564999+d3xt3er@users.noreply.github.com>
2020-03-26 17:41:13 -0300

    Atualizações
    Back-End finalizado

---
+++ Back-End/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+const port = 3333;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+    console.log('Servidor iniciado em http://localhost:3333');
+});
